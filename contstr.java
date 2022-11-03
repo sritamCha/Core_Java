8755dd803caf6782d7c455bc279d3e01958ea2ff
Sritam Chauhan <116060820+sritamCha@users.noreply.github.com>
2022-11-03 12:00:15 +0530

    Add files via upload

---
+++ contstr.java
@@ -0,0 +1,14 @@
+package CoreJava;
+
+public class contstr {
+	final int i=9;
+	void kk() {
+		//i=6;
+		System.out.println(i);
+	}
+public static void main(String[]args) {
+	contstr con=new contstr();
+	con.kk();
+	
+}
+}
---
+++ sumodd.java
@@ -0,0 +1,14 @@
+public class sumodd {
+public static void main(String args[]) {
+		int sum=0;
+		for(int i=1;i<100;i++) {
+			if(i%2!=0)
+			{
+				sum=sum+i;
+			}
+		}
+		System.out.println("'the sum of 100 odd number are "+sum);
+	}
+	}
+
+
